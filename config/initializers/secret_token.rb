a31cd6fde70d4901b1cd4fc0c5041850c44f448b
Kin <winterheat@gmail.com>
2011-04-14 04:32:48 -0700

    ok

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MymyBlog8::Application.config.secret_token = '0d6aec202b43c63f80c9b4668030edaeb0ee8ed33791f2cb46a628690b8e3796da1c74fded1f6c10d1eb79f95b86aafe7068c646ec51e76aa2baf3d89e1fecb7'
